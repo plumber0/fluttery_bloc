86391ee72b5a70b5e09758a506684596d3bd268b
Seugwoo Yoo <seungwoo@deepnatural.ai>
2022-12-08 10:02:08 +0900

    seperate stream script

---
+++ lib/main-stream.dart
@@ -0,0 +1,17 @@
+Stream<int> boatStream() async* {
+  for (int i = 1; i <= 10; i++) {
+    print("SENT boat no. " + i.toString());
+    await Future.delayed(Duration(seconds: 2));
+    yield i;
+  }
+}
+
+void main() async {
+  Stream<int> stream = boatStream();
+
+  stream.listen((receivedData) {
+    print('RECEIVED boat no. ' + receivedData.toString());
+  });
+
+  print('main loop over');
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,66 @@
-Stream<int> boatStream() async* {
-  for (int i = 1; i <= 10; i++) {
-    print("SENT boat no. " + i.toString());
-    await Future.delayed(Duration(seconds: 2));
-    yield i;
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
 
-void main() async {
-  Stream<int> stream = boatStream();
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
 
-  stream.listen((receivedData) {
-    print('RECEIVED boat no. ' + receivedData.toString());
-  });
+  final String title;
 
-  print('main loop over');
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
