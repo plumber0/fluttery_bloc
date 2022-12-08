952b9dc5b4314d0e281c56cce57d6809047e7343
Seugwoo Yoo <seungwoo@deepnatural.ai>
2022-12-08 14:03:00 +0900

    introduce test

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'counter_cubit.dart';
 
-class CounterState {
+class CounterState extends Equatable {
   int counterValue;
   bool wasIncremented;
 
@@ -9,4 +9,7 @@ class CounterState {
     @required this.counterValue,
     this.wasIncremented,
   });
+
+  @override
+  List<Object> get props => [this.counterValue, this.wasIncremented];
 }
