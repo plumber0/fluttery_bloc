1b42980564f2effbfebe21658fdb0543d210e4cd
Seugwoo Yoo <seungwoo@deepnatural.ai>
2022-12-08 16:37:00 +0900

    introduce Anonymous Routing

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: HomeScreen(
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BlocProvider(
+        create: (context) => CounterCubit(),
+        child: HomeScreen(
           title: 'Flutter Demo Home Page',
           color: Colors.redAccent,
         ),
--- lib/presentation/screen/second_screen.dart
+++ lib/presentation/screen/second_screen.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttery_bloc/logic/cubit/counter_cubit.dart';
-import 'package:fluttery_bloc/presentation/screen/second_screen.dart';
 
-class HomeScreen extends StatefulWidget {
-  HomeScreen({Key key, this.title, this.color}) : super(key: key);
+class SecondScreen extends StatefulWidget {
+  SecondScreen({Key key, this.title, this.color}) : super(key: key);
 
   final String title;
   final Color color;
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  _SecondScreenState createState() => _SecondScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,16 +75,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             MaterialButton(
               color: widget.color,
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (_) => BlocProvider.value(
-                          value: BlocProvider.of<CounterCubit>(context),
-                          child: SecondScreen(
-                            title: 'Second Screen',
-                            color: Colors.green,
-                          ),
-                        )));
-              },
+              onPressed: () {},
               child: Text('Go to Second Screen'),
             ),
           ],
