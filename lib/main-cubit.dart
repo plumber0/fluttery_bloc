c9dc0ecb7ca7a844d2a7ca671cbfb676ce6af46d
Seugwoo Yoo <seungwoo@deepnatural.ai>
2022-12-08 10:59:18 +0900

    rename sample cubit because of duplicated import

--- lib/main-cubit.dart
+++ lib/main-cubit.dart
@@ -1,14 +1,14 @@
 import 'package:bloc/bloc.dart';
 
-class CounterCubit extends Cubit<int> {
-  CounterCubit() : super(0);
+class CounterCubit2 extends Cubit<int> {
+  CounterCubit2() : super(0);
 
   void increment() => emit(state + 1);
   void decrement() => emit(state - 1);
 }
 
 void main() {
-  final cubit = CounterCubit();
+  final cubit = CounterCubit2();
 
   print(cubit.state);
   cubit.increment();
